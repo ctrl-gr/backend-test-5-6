61d06e0f88024a22cb657a2638b2d8d7ff610c96
ctrl-gr <giulia@giuliaredolfi.it>
2023-04-17 17:06:23 +0200

    HTML generator

--- src/main/java/HTMLDifferences.java
+++ src/main/java/HTMLDifferences.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class HTMLDifferences {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import difflib.Delta;
+import difflib.DiffUtils;
+import difflib.Patch;
+
+public class HTMLDifferences {
+    public static void main(String[] args) throws Exception {
+
+        List<String> input1 = readFile("src/main/resources/input3.html");
+        List<String> input2 = readFile("src/main/resources/input4.html");
+
+
+        Patch patch = DiffUtils.diff(input1, input2);
+        List<Delta> deltas = patch.getDeltas();
+
+
+        StringBuilder rulesBuilder = new StringBuilder();
+        for (Delta delta : deltas) {
+            switch (delta.getType()) {
+                case INSERT:
+                    rulesBuilder.append("INSERT " + delta.getRevised() + "\n");
+                    break;
+                case DELETE:
+                    rulesBuilder.append("REMOVE " + delta.getOriginal() + "\n");
+                    break;
+                case CHANGE:
+                    rulesBuilder.append("INSERT " + delta.getRevised() + "\n");
+                    rulesBuilder.append("REMOVE " + delta.getOriginal() + "\n");
+                    break;
+
+            }
+        }
+
+
+        FileWriter writer = new FileWriter("output.txt");
+        writer.write(rulesBuilder.toString());
+        writer.close();
+    }
+
+    private static List<String> readFile(String path) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        List<String> lines = new ArrayList<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        reader.close();
+        return lines;
+    }
 }
--- src/main/java/HTMLdiffGenerator.java
+++ src/main/java/HTMLdiffGenerator.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class HTMLdiffGenerator {
+import java.io.*;
+
+
+public class HTMLdiffGenerator {
+    public static void main(String[] args) throws Exception {
+        String inputFilePath = "src/main/resources/input4.html";
+        String outputFilePath = "src/main/resources/output.html";
+        String rulesFilePath = "output.txt";
+
+        try {
+
+            BufferedReader inputReader = new BufferedReader(new FileReader(inputFilePath));
+            StringBuilder inputBuilder = new StringBuilder();
+            String line;
+            while ((line = inputReader.readLine()) != null) {
+                inputBuilder.append(line);
+            }
+            inputReader.close();
+
+            String input = inputBuilder.toString();
+
+
+            BufferedReader rulesReader = new BufferedReader(new FileReader(rulesFilePath));
+            String rule;
+            while ((rule = rulesReader.readLine()) != null) {
+                String[] tokens = rule.split("\\[|\\]");
+
+
+                String command = tokens[0];
+                String[] positionAndSize = tokens[1].split(", ");
+                int position = Integer.parseInt(positionAndSize[1].split(": ")[1]);
+                int size = Integer.parseInt(positionAndSize[0].split(": ")[1]);
+
+                String[] lines = tokens[2].split(": ")[0].split(", ");
+                String replacement = String.join("", lines);
+
+                if (command.equals("INSERT ")) {
+                    input = input.substring(0, position) + replacement + input.substring(position);
+                } else if (command.equals("REMOVE ")) {
+                    int endPosition = position + size;
+                    input = input.substring(0, position) + input.substring(endPosition);
+                } else if (command.equals("CHANGE")) {
+                    int endPosition = position + size;
+                    input = input.substring(0, position) + replacement + input.substring(endPosition);
+                }
+            }
+            rulesReader.close();
+
+
+            BufferedWriter outputWriter = new BufferedWriter(new FileWriter(outputFilePath));
+            outputWriter.write(input);
+            outputWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
